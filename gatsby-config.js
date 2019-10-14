1df5f0c34e216d0438de52b9c581a7da00d96002
Harrison <harrisonhayes97@yahoo.com>
2019-10-14 00:10:27 -0500

    make edits to gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `The Harrison Hayes`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    author: `Harrison Hayes`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
